011451b2247ebab49d0a516fccfe3b247576ee0a
김태현 <taehyk528@gmail.com>
2022-08-21 01:22:46 +0900

    [#1] 프로젝트 초기 세팅

---
+++ DDAK/DDAK/Application/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  DDAK
+//
+//  Created by taekki on 2022/08/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ DDAK/DDAK/Presentation/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DDAK
+//
+//  Created by taekki on 2022/08/21.
+//
+
+import UIKit
+
+import ReactorKit
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
