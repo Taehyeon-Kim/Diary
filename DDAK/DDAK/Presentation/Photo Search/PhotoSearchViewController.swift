d2c3162a2779fd5f2887e7f09edffcd5ac02b399
김태현 <taehyk528@gmail.com>
2022-08-22 00:18:36 +0900

    [#2] 사진 검색 화면 파일 준비

---
+++ DDAK/DDAK/Presentation/Photo Search/PhotoSearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoSearchViewController.swift
+//  DDAK
+//
+//  Created by taekki on 2022/08/22.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class PhotoSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ DDAK/DDAK/Presentation/Photo Search/View/PhotoSearchView.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoSearchView.swift
+//  DDAK
+//
+//  Created by taekki on 2022/08/22.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class PhotoSearchView: UIView {
+    
+}
